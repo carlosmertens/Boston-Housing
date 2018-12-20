faf9df82ec19d7e47444a9575d87af4b837a8f06
Carlos Mertens <carlos.e.mertens.m@gmail.com>
2018-12-20 22:45:42 +0100

    Start Python file to train model

---
+++ train.py
@@ -0,0 +1,14 @@
+# PROGRAMMER: Carlos Mertens
+# DATE CREATED: (DD/MM/YY) - 20/12/18
+# REVISED DATE: (DD/MM/YY) - Not revise it yet
+# PURPOSE: Build an optimal supervised learner model based on a statistical analysis.
+#           Load, split, prepare can calculate statistics on the data.
+#           Perform metric evaluation, grid search. Train optimal model 
+#
+# USAGE: Use argparse Expected Call with <> indicating expected user input:
+#           python check_images.py --dir <directory with data> --arch <model>
+#             --dogfile <file that contains dognames>
+#   Example call:
+#    python train.py --dir Data/housing.csv
+
+# Imports
