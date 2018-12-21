14ca42a1e6a3a7f32a53c81fb8f9d0a215211d6c
Carlos Mertens <carlos.e.mertens.m@gmail.com>
2018-12-21 10:56:56 +0100

    Create get_data function for train.py

--- train.py
+++ train.py
@@ -17,3 +17,29 @@ import pandas as pd
 from sklearn.model_selection import ShuffleSplit, train_test_split, GridSearchCV
 from sklearn.metrics import r2_score, make_scorer
 from sklearn.tree import DecisionTreeRegressor
+
+def get_data(path):
+    """Load csv file and get features and target.
+    
+    Function to load csv file with the Boston housing data using Pandas. 
+    Separate price column to be the target and the other columns to be the features.
+    
+    Parameters:
+     path - Full path to the csv file with the dataset
+    Returns:
+     data - Full dataframe
+     prices - Column with datapoints to be the targets
+     features - Dataframe without the target column to be the features.
+    """
+    # Load the Boston housing dataset and separate features and target values
+    data = pd.read_csv(path)
+    prices = data['MEDV']
+    features = data.drop('MEDV', axis=1)
+
+    # Print shape of dataframe
+    print("** Boston housing dataset has {} datapoints with {} variables each.".format(*data.shape))
+
+    return data, prices, features
+
+
+data, prices, features = get_data('Data/housing.csv')
